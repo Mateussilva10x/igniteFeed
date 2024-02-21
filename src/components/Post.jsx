452bb580a489f6ea9c895bef1c4b6d2b422a7479
Mateus Silva <mateussilva651@gmail.com>
2024-02-21 14:00:22 -0300

    feat(post): create the post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,43 @@
+import styles from "./Post.module.css";
+
+function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://avatars.githubusercontent.com/u/40676870?v=4"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Mateus Silva</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="21 de Fevereiro às 11:30h" dateTime="2024-02-21 11:30:00">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento{" "}
+        </p>
+        da Rocketseat. O nome do projeto é DoctorCare 🚀
+        <p>
+          <a href="#">jane.design/doctorcare </a>
+        </p>
+        <p>
+          <a href="#">#novoprojeto </a>
+          <a href="">#nlw </a>
+          <a href="#">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
+
+export default Post;
