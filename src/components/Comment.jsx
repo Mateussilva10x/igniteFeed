d159f02781d3f4010e906b25b9297e1133fe57e4
Mateus Silva <mateussilva651@gmail.com>
2024-04-16 20:00:32 -0300

    add the like click in the comments

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,10 +2,17 @@
 import { ThumbsUp, Trash } from "phosphor-react";
 import styles from "./Comment.module.css";
 import Avatar from "./Avatar";
+import { useState } from "react";
 
 function Comment({ content, onDeleteComment }) {
+  const [likeCount, setLikeCount] = useState(0);
+
   const handleDeleteComment = () => {
     onDeleteComment(content);
+  };
+
+  const handleLikeComment = () => {
+    setLikeCount(likeCount + 1);
   };
 
   return (
@@ -34,9 +41,9 @@ function Comment({ content, onDeleteComment }) {
           <p>{content}</p>
         </div>
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
